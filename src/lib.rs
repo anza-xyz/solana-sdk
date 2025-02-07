solana-labs/solana

Move system_transaction out of src/

src/lib.rs
@@ -66,7 +66,7 @@ pub mod sigverify_stage;
 pub mod storage_stage;
 pub mod store_ledger_stage;
 pub mod streamer;
-pub mod system_transaction;
+pub mod test_tx;
 pub mod thin_client;
 pub mod tpu;
 pub mod tpu_forwarder;

anza-xyz/solana-sdk

src/lib.rs
@@ -16,6 +16,7 @@ pub mod signature;
 pub mod storage_program;
 pub mod system_instruction;
 pub mod system_program;
+pub mod system_transaction;
 pub mod timing;
 pub mod token_program;
 pub mod transaction;