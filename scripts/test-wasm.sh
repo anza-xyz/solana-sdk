b0fb7db244a9aa1dd0e42e3affa3d890487daae2
Lucas <lucas.tnagel@gmail.com>
2025-04-11 17:38:36 -0300

    Rename crate

--- scripts/test-wasm.sh
+++ scripts/test-wasm.sh
@@ -5,10 +5,6 @@ here="$(dirname "$0")"
 src_root="$(readlink -f "${here}/..")"
 cd "${src_root}"
 
-for dir in program-wasm sdk ; do
-  (
-    cd "$dir"
-    npm install
-    npm test
-  )
-done
+cd sdk-wasm
+npm install
+npm test
