7e19ca4283b0208a06843593d805563e24e3f13a
yihau <yihau.chen@icloud.com>
2025-11-12 23:11:49 +0800

    ci: update test-frozen-abi script

--- scripts/test-frozen-abi.sh
+++ scripts/test-frozen-abi.sh
@@ -5,4 +5,8 @@ here="$(dirname "$0")"
 src_root="$(readlink -f "${here}/..")"
 cd "${src_root}"
 
-./cargo nightly test --features frozen-abi --lib -- test_abi_ --nocapture
+packages=$(./cargo nightly metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.features | has("frozen-abi")) | .name')
+for package in $packages; do
+  echo "--- ./cargo nightly test -p $package --features frozen-abi --lib -- test_abi_ --nocapture"
+  ./cargo nightly test -p "$package" --features frozen-abi --lib -- test_abi_ --nocapture
+done
