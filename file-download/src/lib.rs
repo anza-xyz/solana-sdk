aa50a2506fa74a275e4ccc2d11618207121b4090
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-12-01 15:40:52 -0500

    Optimize file download with buffered writer
    Specify a buffer of 8MB instead of the default 8kb. This increases download speed to be similar to wget.

--- file-download/src/lib.rs
+++ file-download/src/lib.rs
@@ -229,7 +229,12 @@ pub fn download_file_with_headers<'a, 'b, S: AsRef<str>>(
     };
 
     File::create(&temp_destination_file)
-        .and_then(|mut file| std::io::copy(&mut source, &mut file))
+        .and_then(|mut file| {
+            std::io::copy(
+                &mut source,
+                &mut BufWriter::with_capacity(8_000_000, &mut file),
+            )
+        })
         .map_err(|err| format!("Unable to write {temp_destination_file:?}: {err:?}"))?;
 
     source.progress_bar.finish_and_clear();
