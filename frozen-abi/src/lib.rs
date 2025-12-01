9af843b60db97b8e060e6bd041a57c5d43ff4192
marc <marcinbieszke@gmail.com>
2025-12-01 13:30:57 +0100

    add edition 2024 lints

--- frozen-abi/src/lib.rs
+++ frozen-abi/src/lib.rs
@@ -1,6 +1,13 @@
 #![allow(incomplete_features)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(feature = "frozen-abi", feature(specialization))]
+// Activate some of the Rust 2024 lints to make the future migration easier.
+#![warn(if_let_rescope)]
+#![warn(keyword_idents_2024)]
+#![warn(rust_2024_incompatible_pat)]
+#![warn(tail_expr_drop_order)]
+#![warn(unsafe_attr_outside_unsafe)]
+#![warn(unsafe_op_in_unsafe_fn)]
 
 // Allows macro expansion of `use ::solana_frozen_abi::*` to work within this crate
 extern crate self as solana_frozen_abi;
