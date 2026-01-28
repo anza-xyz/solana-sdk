c60a058714b8bbf3a5cbb0e251b39e6a906b0098
Jon C <me@jonc.dev>
2026-01-28 15:42:06 +0100

    Ok, really remove the comment

--- rent/src/lib.rs
+++ rent/src/lib.rs
@@ -173,9 +173,6 @@ impl Rent {
     /// Returns `ProgramError::InvalidArgument` if `data_len` exceeds the maximum
     /// permitted data length or if the `lamports_per_byte` is too large based on
     /// the `exemption_threshold`, which would cause an overflow.
-    //
-    // Note: Clippy suggests collapsing the `if` statements, but they are kept
-    // separate since it is more CU-efficient this way.
     #[inline(always)]
     pub fn try_minimum_balance(&self, data_len: usize) -> Option<u64> {
         if data_len as u64 > MAX_PERMITTED_DATA_LENGTH {
