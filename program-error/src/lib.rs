7c71f4d510590c37b7f5990c7c0a36b118c6c56c
Alexander Meißner <AlexanderMeissner@gmx.net>
2026-08-12 12:42:52 +0000

    Adds ProgramError::BailOut.

--- program-error/src/lib.rs
+++ program-error/src/lib.rs
@@ -45,6 +45,7 @@ pub const INVALID_ACCOUNT_OWNER: u64 = to_builtin!(23);
 pub const ARITHMETIC_OVERFLOW: u64 = to_builtin!(24);
 pub const IMMUTABLE: u64 = to_builtin!(25);
 pub const INCORRECT_AUTHORITY: u64 = to_builtin!(26);
+pub const BAIL_OUT: u64 = to_builtin!(27);
 // Warning: Any new error codes added here must also be:
 // - Added to the below conversions
 // - Added as an equivalent to ProgramError and InstructionError
@@ -84,6 +85,7 @@ pub enum ProgramError {
     ArithmeticOverflow,
     Immutable,
     IncorrectAuthority,
+    BailOut,
 }
 
 impl core::error::Error for ProgramError {}
@@ -141,6 +143,8 @@ impl fmt::Display for ProgramError {
              => f.write_str("Account is immutable"),
             ProgramError::IncorrectAuthority
              => f.write_str("Incorrect authority provided"),
+            ProgramError::BailOut
+             => f.write_str("Block production bailed out"),
         }
     }
 }
@@ -224,6 +228,7 @@ impl ProgramError {
             Self::ArithmeticOverflow => "Error: ArithmeticOverflow",
             Self::Immutable => "Error: Immutable",
             Self::IncorrectAuthority => "Error: IncorrectAuthority",
+            Self::BailOut => "Error: BailOut",
         }
     }
 }
@@ -262,6 +267,7 @@ impl From<ProgramError> for u64 {
             ProgramError::ArithmeticOverflow => ARITHMETIC_OVERFLOW,
             ProgramError::Immutable => IMMUTABLE,
             ProgramError::IncorrectAuthority => INCORRECT_AUTHORITY,
+            ProgramError::BailOut => BAIL_OUT,
             ProgramError::Custom(error) => {
                 if error == 0 {
                     CUSTOM_ZERO
@@ -304,6 +310,7 @@ impl From<u64> for ProgramError {
             ARITHMETIC_OVERFLOW => Self::ArithmeticOverflow,
             IMMUTABLE => Self::Immutable,
             INCORRECT_AUTHORITY => Self::IncorrectAuthority,
+            BAIL_OUT => Self::BailOut,
             _ => Self::Custom(error as u32),
         }
     }
