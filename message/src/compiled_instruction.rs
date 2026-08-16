a96df228ffb8d403a89cf9b9a6e6fcbaf6085930
Raushan Singh <raushansinghrajpoot687@gmail.com>
2026-08-16 10:09:57 +0000

    feat(compiled-instruction): add fallible wincode constructor

--- message/src/compiled_instruction.rs
+++ message/src/compiled_instruction.rs
@@ -49,17 +49,34 @@ impl Sanitize for CompiledInstruction {}
 
 impl CompiledInstruction {
     #[cfg(feature = "wincode")]
+    /// Create a new instruction from a value, encoded with [`wincode`].
+    ///
+    /// This is the infallible version of [`CompiledInstruction::try_new`], and will
+    /// panic if serialization fails.
     pub fn new<T: wincode::Serialize<Src = T>>(
         program_ids_index: u8,
         data: &T,
         accounts: Vec<u8>,
     ) -> Self {
-        let data = wincode::serialize(data).unwrap();
-        Self {
+        Self::try_new(program_ids_index, data, accounts)
+            .expect("Failed to serialize instruction data")
+    }
+
+    #[cfg(feature = "wincode")]
+    /// Fallibly create a new instruction from a value, encoded with [`wincode`].
+    ///
+    /// [`wincode`]: https://docs.rs/wincode/latest/wincode/
+    pub fn try_new<T: wincode::Serialize<Src = T>>(
+        program_ids_index: u8,
+        data: &T,
+        accounts: Vec<u8>,
+    ) -> Result<Self, wincode::Error> {
+        let data = wincode::serialize(data)?;
+        Ok(Self {
             program_id_index: program_ids_index,
             accounts,
             data,
-        }
+        })
     }
 
     pub fn new_from_raw_parts(program_id_index: u8, data: Vec<u8>, accounts: Vec<u8>) -> Self {
@@ -72,5 +89,59 @@ impl CompiledInstruction {
 
     pub fn program_id<'a>(&self, tx_accounts: &'a [Address]) -> &'a Address {
         &tx_accounts[self.program_id_index as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(feature = "wincode")]
+    mod wincode_tests {
+        use super::*;
+
+        struct FailingWincode;
+        unsafe impl wincode::SchemaWrite<wincode::config::DefaultConfig> for FailingWincode {
+            type Src = Self;
+
+            fn size_of(_src: &Self::Src) -> wincode::WriteResult<usize> {
+                Err(wincode::WriteError::Custom("intentional failure"))
+            }
+
+            fn write(
+                _writer: impl wincode::io::Writer,
+                _src: &Self::Src,
+            ) -> wincode::WriteResult<()> {
+                Err(wincode::WriteError::Custom("intentional failure"))
+            }
+        }
+
+        #[test]
+        fn test_try_new_failure() {
+            let result = CompiledInstruction::try_new(0, &FailingWincode, alloc::vec![]);
+            assert!(result.is_err());
+        }
+
+        #[test]
+        #[should_panic(expected = "Failed to serialize instruction data")]
+        fn test_new_panic() {
+            CompiledInstruction::new(0, &FailingWincode, alloc::vec![]);
+        }
+
+        #[derive(wincode::SchemaWrite)]
+        struct SuccessWincode {
+            value: u64,
+        }
+
+        #[test]
+        fn test_new_success() {
+            let data = SuccessWincode { value: 42 };
+            let instr = CompiledInstruction::new(0, &data, alloc::vec![]);
+            let expected_data = wincode::serialize(&data).unwrap();
+            assert_eq!(instr.data, expected_data);
+
+            let try_instr = CompiledInstruction::try_new(0, &data, alloc::vec![]).unwrap();
+            assert_eq!(try_instr.data, expected_data);
+        }
     }
 }
